50dbb7e788f97997f3ab0ff20db13b98edb3f2a4
Ruslan <_>
2023-02-14 04:00:25 +0700

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+
+    public int add(String name, String num) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        String var1 = "Ольга";
+        String var2 = "Алексей";
+        String var3 = "89651258844";
+        String var4 = "34552345677";
+        String var5 = "89033103365";
+        int var6 = 1;
+        int var7 = 2;
+        assertEquals(var6,phoneBook.add(var1, var3));
+        assertEquals(var7,phoneBook.add(var2, var4));
+        assertEquals(var7,phoneBook.add(var1, var5));
+    }
+
 }
