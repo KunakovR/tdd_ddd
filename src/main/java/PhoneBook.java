dbb7d79dee53ed604dac57e5d50140fe6fcd7ffd
Ruslan <_>
2023-02-14 04:06:35 +0700

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    Map<String, String> storage = new HashMap<>();
+
     public int add(String name, String num) {
-        return 0;
+        int val = 0;
+        if (storage.containsKey(name) == true) {
+            for (String sb : storage.keySet()){
+                val++;
+            }
+            return val;
+        }
+        else storage.put(name, num);
+        for (String sb2 : storage.keySet()){
+            val++;
+        }
+        return val;
     }
 }
